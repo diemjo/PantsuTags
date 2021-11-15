ea7fa056687686f645e4777b5c4f0f95aad86190
Joopon <joopon.jnw@gmail.com>
2021-11-15 23:06:25 +0100

    clean sauce/mod.rs

--- src/sauce/mod.rs
+++ src/sauce/mod.rs
@@ -12,28 +12,13 @@ mod tests {
     use crate::sauce;
 
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-
-    #[test]
-    //#[ignore]
     fn find_sauce() {
         let path = Path::new("test.png");
-        /*let file = match File::open(&path) {
-            Err(why) => panic!("couldn't open {}: {}", path.display(), why),
-            Ok(file) => file,
-        };
-        */
         let sauces = sauce::sauce_finder::find_sauce(&path).unwrap();
         println!("found sauces:");
         for s in sauces {
             println!("link: {}, similarity: {}", s.link, s.similarity);
         }
-        /*let res = sauce::sauce_finder::find_sauce(&path);
-        if let Err(err) = res {
-            println!("error:\n{}", err.to_string());
-        }*/
     }
 
     #[test]
