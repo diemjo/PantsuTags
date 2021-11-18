05e8dace89681e7144f829325c53263b0db5bc77
Joris Diem <j.diem@me.com>
2021-11-18 21:34:39 +0100

    don't throw error when hard linked file already exists

--- src/file_handler/import.rs
+++ src/file_handler/import.rs
@@ -1,3 +1,4 @@
+use std::io::ErrorKind::AlreadyExists;
 use std::path::PathBuf;
 
 use crate::common::error::Error;
@@ -10,8 +11,12 @@ pub fn import_file(lib: &str, file: &str, new_filename: &str) -> Result<(), Erro
 
     let mut new_path = PathBuf::from(&lib_path);
     new_path.push(new_filename);
-    std::fs::hard_link(PathBuf::from(file), new_path).or_else(|err|
-        Err(Error::HardLinkError(err, String::from(file)))
-    )?;
+    std::fs::hard_link(PathBuf::from(file), new_path).or_else(|err| {
+        if err.kind()==AlreadyExists {
+            Ok(())
+        } else {
+            Err(Error::HardLinkError(err, String::from(file)))
+        }
+    })?;
     Ok(())
 }
