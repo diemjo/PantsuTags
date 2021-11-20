ef4c0949463972fea5b0e4a5adc8e938f32d6cdc
Joris Diem <j.diem@me.com>
2021-11-21 00:43:00 +0100

    renamed bin in build.rs

--- build.rs
+++ build.rs
@@ -5,5 +5,5 @@ include!("src/cli/mod.rs");
 fn main() {
     let mut app = Args::clap();
     std::fs::create_dir_all("./target/").unwrap();
-    app.gen_completions("./pantsu-tags", Bash, "./target/");
+    app.gen_completions("pantsu-tags", Bash, "./target/");
 }
