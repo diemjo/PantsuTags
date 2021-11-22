6cbd3425010d87bdce9603a138cd0b57e2744174
Joris Diem <j.diem@me.com>
2021-11-22 18:03:10 +0100

    added imagefile tag for inexistent sauce

--- src/common/image_file.rs
+++ src/common/image_file.rs
@@ -5,3 +5,22 @@ pub struct ImageFile {
     pub filename: String,
     pub file_source: Option<String>
 }
+
+impl ImageFile {
+    pub fn sauce_is_nonexistent(&self) -> bool {
+        return match &self.file_source {
+            Some(sauce) => sauce.eq(NONEXISTENT_FLAG),
+            None => false
+        }
+    }
+
+    pub fn sauce_is_not_checked_yet(&self) -> bool {
+        return match &self.file_source {
+            Some(_) => false,
+            None => true
+        }
+    }
+}
+
+pub const NONEXISTENT_FLAG: &str =
+    "NONEXISTENT";
